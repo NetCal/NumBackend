043d58fe22c1b707bf425ac72144e0ec89f860d5
sbondorf <steffen@s-bondorf.de>
2018-08-18 08:07:09 -0400

    Replace use of deprecated Float(double) constructor

--- src/main/java/de/uni_kl/cs/discodnc/numbers/implementations/RealSinglePrecision.java
+++ src/main/java/de/uni_kl/cs/discodnc/numbers/implementations/RealSinglePrecision.java
@@ -240,12 +240,12 @@ public class RealSinglePrecision implements Num {
 
     // Compare to other number: >, >=, =, <=, <
     public boolean gt(Num num) {
-        float num_float = new Float(num.doubleValue());
+        float num_float = Double.valueOf(num.doubleValue()).floatValue();
         return value > num_float;
     }
     
     public boolean geq(Num num) {
-        float num_float = new Float(num.doubleValue());
+        float num_float = Double.valueOf(num.doubleValue()).floatValue();
         return value >= num_float;
     }
     
